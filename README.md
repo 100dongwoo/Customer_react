4df0573ed4e5d44e5373fe9fb58c84567c3b2b36
BAEK DONGWOO <qorehddn123@naver.com>
2020-08-12 17:16:08 +0900

    Connect server-client

---
+++ README.md
@@ -0,0 +1,24 @@
+## Customer Mangement
+
+using system
+1. material UI
+2. 
+
+## Important
+
+##### 1. Nodejs Server Connect <br>
+![nodejs server connect](./image/nodejs서버구축.png)
+
+##### 2. proxy :  3000 client connect 5000 server <br>
+
+ "proxy": "http://localhost:5000/   (client package.json)
+ -> Client 3000port Connect to Server 5000port
+ 
+
+
+
+
+
+<br><br>
+ tutorial = >  [Link](https://www.youtube.com/watch?v=zug4VBcZOrI&list=PLRx0vPvlEmdD1pSqKZiTihy5rplxecNpz&index=8). 
+ From : ndb796(github ID)
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Customer from "./component/Customer";
@@ -8,78 +8,95 @@ import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import Paper from "@material-ui/core/Paper";
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 
 const styles = (theme) => ({
-  root: {
-    width: "100%",
-    marginTop: theme.spacing.unit,
-    overflowX: "auto",
-  },
-  table: {
-    minWidth: 1080,
-  },
+    root: {
+        width: "100%",
+        marginTop: theme.spacing.unit,
+        overflowX: "auto",
+    },
+    table: {
+        minWidth: 1080,
+    },
 });
 
 function App(props) {
-  const customers = [
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "나동빈",
-      birthday: "961212",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "나동3빈",
-      birthday: "961212",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "나2동빈",
-      birthday: "961212",
-      gender: "남자",
-      job: "대학생",
-    },
-  ];
-  const { classes } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map((c, index) => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+
+    const [Customers, setCustomers] = useState("")
+
+    useEffect(() => {
+        callAPI()
+            .then(res => setCustomers(res))
+            .catch(err=>console.log(err))
+    })
+
+
+    const callAPI = async () => {
+        const response = await fetch('/api/customers');
+        const body = await response.json();
+        return body
+    }
+
+
+    const customers = [
+        {
+            id: 1,
+            image: "https://placeimg.com/64/64/1",
+            name: "나동빈",
+            birthday: "961212",
+            gender: "남자",
+            job: "대학생",
+        },
+        {
+            id: 2,
+            image: "https://placeimg.com/64/64/2",
+            name: "나동3빈",
+            birthday: "961212",
+            gender: "남자",
+            job: "대학생",
+        },
+        {
+            id: 3,
+            image: "https://placeimg.com/64/64/3",
+            name: "나2동빈",
+            birthday: "961212",
+            gender: "남자",
+            job: "대학생",
+        },
+    ];
+    const {classes} = props;
+    return (
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>번호</TableCell>
+                        <TableCell>이미지</TableCell>
+                        <TableCell>이름</TableCell>
+                        <TableCell>생년월일</TableCell>
+                        <TableCell>성별</TableCell>
+                        <TableCell>직업</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {Customers ? Customers.map((c, index) => {
+                        return (
+                            <Customer
+                                key={c.id}
+                                id={c.id}
+                                image={c.image}
+                                name={c.name}
+                                birthday={c.birthday}
+                                gender={c.gender}
+                                job={c.job}
+                            />
+                        );
+                    }):" "}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
 }
 
 export default withStyles(styles)(App);
